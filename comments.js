dfa0cedb4b82e3303a62d5412999a611c632ead1
LA <lucas@censorship.rip>
2024-05-24 18:49:18 +0930

    test

--- comments.js
+++ comments.js
@@ -1,1 +1,6 @@
 document.write("<zap-threads anchor=\"\"\/>");
+window.addEventListener('load', function() {
+    const currentURL = window.location.href;
+    const anchorElement = document.querySelector('zap-threads');
+    anchorElement.setAttribute('anchor', currentURL);
+  });
