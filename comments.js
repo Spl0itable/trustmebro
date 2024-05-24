20ba6b03cf28602bea351e217b77460f1d8bdc17
LA <lucas@censorship.rip>
2024-05-24 18:52:29 +0930

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,6 +1,7 @@
-document.write("<zap-threads anchor=\"\"\/>");
+document.write("<div class=\"note_wrapper\"><zap-threads anchor=\"\"\/><\/div>");
 window.addEventListener('load', function() {
     const currentURL = window.location.href;
     const anchorElement = document.querySelector('zap-threads');
     anchorElement.setAttribute('anchor', currentURL);
   });
+  
